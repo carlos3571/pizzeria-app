b4878c795f3b3eb786f12e7969badc2e860400b2
Carlos Andres Orozco Illera <carlosandresorozcoillera@gmail.com>
2025-04-12 14:16:14 -0500

    Se crea las migraciones

---
+++ database/migrations/2025_04_12_183356_create_purchases_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('purchases', function (Blueprint $table) {
+            $table->id();
+            $table->integer('supplier_id');
+            $table->integer('raw_material_id');
+            $table->decimal('quantity', 8, 2);
+            $table->decimal('purchase_price', 8, 2);
+            $table->timestamp('purchase_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('purchases');
+    }
+};
